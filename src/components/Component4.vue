ae7cbe382c34750c71facbea1109345e5f0b3c52
linh20111789 <linh20111789@gmail.com>
2022-08-09 22:01:09 +0700

    add table vue

--- src/components/Component4.vue
+++ src/components/Component4.vue
@@ -1,45 +1,81 @@
 <template>
   <div>
-    <p> Component 4</p>
+    <p>Component 4</p>
   </div>
+  <main class="col-md-6 ms-sm-auto col-lg-8 dashboard-content" style="text-align: left">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="card">
+          <h2>改善項目リスト</h2>
+          <table
+            id="table3"
+            class="table-matrix table table-hover table-striped table-responsive"
+          >
+            <tbody>
+              <tr>
+                <td>01.c.3</td>
+                <td>利害関係者は、コミットしたリソースを実際に手配している</td>
+              </tr>
+              <tr>
+                <td>01.c.4</td>
+                <td>
+                  利害関係者は、テストプロセスに影響を与える側面について積極的に行動を起こしている。これには、テスト対象のテスト作業へのリリース順序やプロジェクトスコープの変更が含まれる。
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
-  <h2>改善項目リスト</h2>
-  <table id="table3" class="table-matrix table table-hover table-striped table-responsive">
-      <thead id="table3header">
-        <tr>
-          <th> Header 1 </th>
-          <th> Header 2 </th>
-        </tr>
-      </thead>    
-      <tbody>
-        <tr>
-            <td>Ram</td>
-            <td>21</td>
-            <td>Male</td>
-            <td>Doctor</td>
-        </tr>
-        <tr>
-            <td>Mohan</td>
-            <td>32</td>
-            <td>Male</td>
-            <td>Teacher</td>
-        </tr>  
-      </tbody>     
+          <h2>改善項目リスト</h2>
+          <table
+            id="table3"
+            class="table-matrix table table-hover table-striped table-responsive"
+          >
+            <tbody>
+              <tr>
+                <td style="width: 5%">01.c.3</td>
+                <td style="width: 95%">
+                  利害関係者は、コミットしたリソースを実際に手配している
+                </td>
+              </tr>
+              <tr>
+                <td>01.c.4</td>
+                <td>利害のリリース順序やプロジェクトスコープの変更が含まれる。</td>
+              </tr>
+            </tbody>
+          </table>
 
-  </table>
-
+          <h2>改善項目リスト</h2>
+          <table
+            id="table3"
+            class="table-matrix table table-hover table-striped table-responsive"
+          >
+            <tbody>
+              <tr>
+                <td>01.c.3</td>
+                <td>
+                  利害関係者は、コミットしたリソースを実際に手配している利害関係者は、コミットしたリソースを実際に手配している利害関係者は、コミットしたリソースを実際に手配している利害関係者は、コミットしたリソースを実際に手配している利害関係者は、コミットしたリソースを実際に手配している
+                </td>
+              </tr>
+              <tr>
+                <td>01.c.4</td>
+                <td>
+                  利害関係者は、テストプロセスに影響を与える側面について積極的に行動を起こしている。これには、テスト対象のテスト作業へのリリース順序やプロジェクトスコープの変更が含まれる。
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
   name: "Component-4",
-  props: ['email'],
+  props: ["email"],
 };
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+/* eslint-disable */
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 createApp(App).mount('#app')
+
+/* eslint-disable */
+// // Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue)
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
